5ded4fdac5c9d175cb43e8f7b6ba147bb2ee885d
Henrik Lissner <git@henrik.io>
2026-07-22 21:18:37 -0400

    feat(modules): add shell.claude
    Sigh.

---
+++ modules/shell/claude.nix
@@ -0,0 +1,32 @@
+# modules/shell/claude.nix
+#
+# AI has become so ubiquitious that I can't avoid installing it just to support
+# clients who (ab)use it.
+
+{ hey, lib, config, options, pkgs, ... }:
+
+with lib;
+with hey.lib;
+let cfg = config.modules.shell.claude;
+in {
+  options.modules.shell.claude = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      claude-code
+    ];
+
+    # Respect XDG, damn it!
+    environment.sessionVariables = {
+      CLAUDE_CONFIG_DIR = "${hey.configDir}/claude";
+    };
+
+    home.configFile."claude/settings.json".text = ''
+      {
+        "env": { "DISABLE_AUTOUPDATED": "1" }
+      }
+    '';
+  };
+}
