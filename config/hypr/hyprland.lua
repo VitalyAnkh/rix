bad771e46324a356b2b0ffcaa4b071b0f56374e2
Henrik Lissner <git@henrik.io>
2026-09-10 20:38:03 -0400

    feat(hypr): bind SUPER+w to cycle between floats/tiles

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -359,6 +359,8 @@ hl.bind("SUPER + CTRL + h",         hl.dsp.focus({ monitor = "l" }))
 hl.bind("SUPER + CTRL + j",         hl.dsp.focus({ monitor = "d" }))
 hl.bind("SUPER + CTRL + k",         hl.dsp.focus({ monitor = "u" }))
 hl.bind("SUPER + CTRL + l",         hl.dsp.focus({ monitor = "r" }))
+-- Cycle between floats and tiles
+hl.bind("SUPER + w", hl.dsp.window.cycle_next({ floating = not hl.get_active_window().floating }))
 
 -- ** Workspaces
 for i = 1, 10 do
