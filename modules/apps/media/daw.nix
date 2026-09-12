6933325661fd4cd51c42976c154c127c7a259d25
Henrik Lissner <git@henrik.io>
2026-09-11 20:21:02 -0400

    fix(daw): plugin paths

--- modules/apps/media/daw.nix
+++ modules/apps/media/daw.nix
@@ -11,26 +11,19 @@
 with lib;
 with hey.lib;
 let cfg = config.modules.apps.media.daw;
+    # Every plugin format I care to support
+    formats = [ "dssi" "ladspa" "lv2" "lxvst" "vst" "vst3" ];
+    pluginPaths = f:
+      genAttrs' formats (format: nameValuePair "${toUpper format}_PATH" (f format));
 in {
   options.modules.apps.media.daw = {
     enable = mkBoolOpt false;
   };
 
   config = mkIf cfg.enable {
-    environment.variables = let
-      makePluginPath = format:
-        (makeSearchPath format [
-          "$HOME/.nix-profile/lib"
-          "/run/current-system/sw/lib"
-          "/etc/profiles/per-user/$USER/lib"
-        ]) + ":$HOME/.config/${format}";
-    in {
-      DSSI_PATH   = makePluginPath "dssi";
-      LADSPA_PATH = makePluginPath "ladspa";
-      LV2_PATH    = makePluginPath "lv2";
-      LXVST_PATH  = makePluginPath "lxvst";
-      VST_PATH    = makePluginPath "vst";
-      VST3_PATH   = makePluginPath "vst3";
+    environment = {
+      profileRelativeSessionVariables = pluginPaths (format: [ "/lib/${format}" ]);
+      variables = pluginPaths (format: [ "$XDG_DATA_HOME/${format}" ]);
     };
 
     services.pipewire.jack.enable = true;
